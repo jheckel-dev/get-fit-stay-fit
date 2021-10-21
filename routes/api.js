383e86ebebf1694f7c1b197f48950d00964aa160
jheckel <jalen.heckel@icloud.com>
2021-10-21 11:58:46 -0700

    added router for adding to an existing workout

--- routes/api.js
+++ routes/api.js
@@ -12,3 +12,19 @@ router.get("api/workouts", (req, res) => {
         res.json(message);
     })
 })
+// add to existing workout
+router.put("api/workouts/:id", ({body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: {exercises: body } },
+        { new: true }
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("api/wor")
