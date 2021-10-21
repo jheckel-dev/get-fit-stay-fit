148f72ddf470f14162ee5b5ff85c9fee83b44295
jheckel <jalen.heckel@icloud.com>
2021-10-21 12:07:26 -0700

    started workout db todo list

--- models/workout.js
+++ models/workout.js
@@ -3,5 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema ({
-    day:
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{}]
 })
+
+
+
+// TODO: add exercise name, distance, weight, sets, reps, length of time
+// turn to JSON
+// length of total workout?
+// export
